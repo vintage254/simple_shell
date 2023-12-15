273d982d24f91d0ee9250c8fa44c55688459a2e5
sambutracy <tracysambut@gmail.com>
2023-12-15 02:51:02 -0800

    path issue

--- path_file.c
+++ path_file.c
@@ -1,4 +1,6 @@
 #include "shell.h"
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <unistd.h>
@@ -13,12 +15,19 @@ char *construct_full_path(const char *token, const char *command)
 {
 	size_t token_len = strlen(token);
 	size_t command_len = strlen(command);
+	size_t full_path_size = token_len + command_len + 2;
 
-	char *full_path = malloc(token_len + 1 + command_len + 1);
+	char *full_path = malloc(full_path_size);
 
 	if (full_path == NULL)
 	{
 		perror("error in malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	if (full_path_size <= token_len + 1 || full_path_size <= command_len + 1)
+	{
+		perror("insufficient space for full path");
 		exit(EXIT_FAILURE);
 	}
 
@@ -36,9 +45,20 @@ char *construct_full_path(const char *token, const char *command)
  */
 void execute_command(char **comargs)
 {
+	char **env = environ;
+
+	printf("Executing command: %s\n", comargs[0]);
+
+	while (*env)
+	{
+		printf("Env: %s\n", *env);
+		env++;
+	}
+
 	if (execvp(comargs[0], comargs) == -1)
 	{
 		perror("execvp failed");
+		fprintf(stderr, "Error code: %d\n", errno);
 		exit(EXIT_FAILURE);
 	}
 
@@ -85,7 +105,10 @@ static int search_in_path(const char *token,
  */
 void handle_path(const char *command, char **comargs)
 {
+	char new_path[1024];
 	char *path = getenv("PATH");
+
+	printf("PATH: %s\n", path);
 
 	/*Check if PATH is not set*/
 	if (path == NULL)
@@ -93,6 +116,9 @@ void handle_path(const char *command, char **comargs)
 		letsprint("problem with the PATH environment .\n");
 		exit(EXIT_FAILURE);
 	}
+
+	snprintf(new_path, sizeof(new_path), "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:%s", path);
+	setenv("PATH", new_path, 1);
 	/* Check if the command contains a '/'*/
 
 	if (strchr(command, '/') != NULL)
